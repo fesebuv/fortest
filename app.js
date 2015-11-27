f426609443e1428fe18627078420539397117e08
Felipe Buenano <fesebuv@gmail.com>
2015-11-27 02:47:23 -0500

    clean up app.js

--- app.js
+++ app.js
@@ -16,84 +16,6 @@ var Utils = function(){
 }();
 
 
-// var FeedApp = function($scope, $http){
-
-// 	$scope.test = 'test';
-
-// 	$scope.page = 1;
-// 	$scope.photos = [];
-
-
-
-// 		$scope.nextPage = function(){
-
-// 			$scope.page = $scope.page+1;
-// 			$scope.getResults();
-
-// 		};
-
-
-// 		$scope.prevPage = function(){
-
-// 			$scope.page = $scope.page-1;
-// 			$scope.getResults();
-
-// 		};
-
-
-
-//     	$scope.getResults = function(){
-
-
-//     		var params = {
-// 				'method': 'flickr.photos.search',
-// 				'page' : $scope.page,
-// 				// 'tags': 'soccer',
-// 				'text': 'soccer',
-// 				'per_page':'10',
-// 				'extras': 'url_o, url_s, url_n',
-// 				'media': 'photos',
-// 				'api_key': 'e8a09d2557f2dc06df1301a8fa31ca07',
-// 				'format': 'json',
-// 				'jsoncallback': 'JSON_CALLBACK'
-// 			}
-
-
-//     		var url = 'https://api.flickr.com/services/rest/' + Utils.getParams(params);
-
-//     		$http.jsonp(url)
-//     			.success(function(response) {
-
-
-// 					// console.info(response);
-
-
-// 					var locPhotos = response.photos.photo || [];
-
-// 					console.log(locPhotos);
-
-// 					$scope.photos = $scope.photos.concat(locPhotos);
-
-// 					// console.dir($scope.photos);
-
-//     		});
-//     	};
-
-//     $scope.getResults();
-
-// }
-
-
-
-
-// $(window).scroll(function(){
-// 	if  ($(window).scrollTop() == $(document).height() - $(window).height()){
-//     	// run our call for pagination
-
-//     	// console.info('lets make');
-//     	// FeedApp.getResults();
-//     }
-// }); 
 
 
 var app = angular.module('myApp', [])
@@ -101,7 +23,6 @@ var app = angular.module('myApp', [])
 .controller('myCtrl', ['$scope', '$http', function ($scope, $http) {
 
 
-// app.controller('FeedCtrl', function($scope, $http) {
 
 
 	$scope.test = 'test';
@@ -110,22 +31,7 @@ var app = angular.module('myApp', [])
 	$scope.photos = [];
 
 
-	// var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=soccer&api_key=e8a09d2557f2dc06df1301a8fa31ca07&format=json&jsoncallback=JSON_CALLBACK';
-
-		// $scope.nextPage = function(){
-
-		// 	$scope.page = $scope.page+1;
-		// 	$scope.getResults();
-
-		// };
-
-
-		// $scope.prevPage = function(){
-
-		// 	$scope.page = $scope.page-1;
-		// 	$scope.getResults();
-
-		// };
+	
 
 
 
@@ -152,16 +58,11 @@ var app = angular.module('myApp', [])
     		$http.jsonp(url)
     			.success(function(response) {
 
-
-					// console.info(response);
-
-
 					var locPhotos = response.photos.photo || [];
 
 					console.log(locPhotos);
 
 					$scope.photos = $scope.photos.concat(locPhotos);
-
 
     		});
     	};
@@ -173,8 +74,6 @@ var app = angular.module('myApp', [])
     	console.info('show overlay');
     	console.info(imgsrc);
     	
-
-    	// image.width = '500px';
     	image.src = imgsrc;
     	overlayLib.openOverlay('#overlay');
 
@@ -195,16 +94,6 @@ var app = angular.module('myApp', [])
         	doc = elem.ownerDocument,
         	win = doc.defaultView || doc.parentWindow;
         
-        // elm.bind('scroll', function() {
-
-        // 	console.info('scroll');
-
-        // 	console.info(raw.scrollTop + raw.offsetHeight);
-
-        //     if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
-        //         scope.$apply(attr.whenScrolled);
-        //     }
-        // });
 
 		$(window).scroll(function(){
 			if  ($(window).scrollTop() == $(document).height() - $(window).height()){
